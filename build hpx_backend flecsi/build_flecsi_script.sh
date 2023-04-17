e4c5aeb8a0967d7547f4159ad13a5da5895c1940
Chuanqiu He <49005493+hcq9102@users.noreply.github.com>
2023-04-17 10:58:04 -0500

    Create build_flecsi_script.sh

---
+++ build hpx_backend flecsi/build_flecsi_script.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -e
+
+
+module load openmpi
+module load gcc/12.2.0
+
+BASE_DIR=$(pwd)
+SPACK_DIR=${BASE_DIR}/spack
+FLECSI_DIR=${BASE_DIR}/flecsi
+
+mkdir -p ${BASE_DIR}
+
+if [ ! -d ${FLECSI_DIR} ]; then
+  git clone --branch task_local https://github.com/STEllAR-GROUP/flecsi2.git ${FLECSI_DIR}
+fi
+if [ ! -d ${SPACK_DIR} ]; then
+  git clone https://github.com/spack/spack.git ${SPACK_DIR}
+  source ${SPACK_DIR}/share/spack/setup-env.sh
+  spack env create -d ${BASE_DIR}
+  spack env activate ${BASE_DIR}
+  sed -i 's/unify: false/unify: true/' ${BASE_DIR}/spack.yaml
+  spack repo add ${FLECSI_DIR}/spack-repo/
+  spack external find ninja cmake openmpi python autoconf automake perl m4 hpx ca-certificates-mozilla
+  spack add googletest
+  spack add flecsi%gcc@12.2.0 backend=hpx +flog +unit ^hpx ^boost@1.80.0
+  spack concretize -f
+  spack install 
+fi
