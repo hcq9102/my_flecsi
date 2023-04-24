6df502b407d1c9b287c7d4a85f63a90724e37485
Chuanqiu He <49005493+hcq9102@users.noreply.github.com>
2023-04-24 10:59:51 -0500

    install flecsi & do ctest in one script

--- build hpx_backend flecsi/build_flecsi_script.sh
+++ build hpx_backend flecsi/build_flecsi_script.sh
@@ -1,12 +1,11 @@
 #!/bin/bash -e
 
 
-module load openmpi
-module load gcc/12.2.0
+module load gcc cmake git 
 
 BASE_DIR=$(pwd)
 SPACK_DIR=${BASE_DIR}/spack
-FLECSI_DIR=${BASE_DIR}/flecsi
+FLECSI_DIR=${BASE_DIR}/flecsi2
 
 mkdir -p ${BASE_DIR}
 
@@ -17,12 +16,20 @@ if [ ! -d ${SPACK_DIR} ]; then
   git clone https://github.com/spack/spack.git ${SPACK_DIR}
   source ${SPACK_DIR}/share/spack/setup-env.sh
   spack env create -d ${BASE_DIR}
-  spack env activate ${BASE_DIR}
-  sed -i 's/unify: false/unify: true/' ${BASE_DIR}/spack.yaml
+  spack env activate -p ${BASE_DIR}
   spack repo add ${FLECSI_DIR}/spack-repo/
-  spack external find ninja cmake openmpi python autoconf automake perl m4 hpx ca-certificates-mozilla
-  spack add googletest
-  spack add flecsi%gcc@12.2.0 backend=hpx +flog +unit ^hpx ^boost@1.80.0
-  spack concretize -f
-  spack install 
+  spack add flecsi%gcc@12.2.0 backend=hpx +flog +unit ^boost@1.80.0
+  spack install --only dependencies flecsi%gcc@12.2.0 backend=hpx +flog +unit ^boost@1.80.0
 fi
+spack env activate -p ${BASE_DIR}
+
+#build flecsi from source with spack env
+cmake -S ${FLECSI_DIR} -B ${FLECSI_DIR}/cmake-build -DFLECSI_BACKEND=hpx -DENABLE_UNIT_TESTS=ON
+cmake --build ${FLECSI_DIR}/cmake-build/ --parallel
+
+#ctest
+cd ${FLECSI_DIR}/cmake-build/
+ctest
+
+#exit spack
+despacktivate
