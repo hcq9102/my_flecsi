e93a868d2f1335921901c63665d5b53886e6080d
Chuanqiu He <49005493+hcq9102@users.noreply.github.com>
2023-05-23 16:24:50 -0500

    Create build_flecsi_spack.sh

---
+++ hpx_backend tests/build_flecsi_spack.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -e
+
+#SBATCH --job-name=flecsi_spack_build
+#SBATCH --output=flecsi_spack_build.out
+#SBATCH --nodes=1
+#SBATCH --time=05:00:00
+#SBATCH --partition=medusa
+
+# set -xe
+#module purge
+module load gcc/12.2.0 cmake/3.23.2 git 
+
+BASE_DIR=$(pwd)
+SPACK_DIR=${BASE_DIR}/spack
+FLECSI_DIR=${BASE_DIR}/flecsi2
+
+mkdir -p ${BASE_DIR}
+
+if [ ! -d ${FLECSI_DIR} ]; then
+  git clone --branch task_local2 https://github.com/STEllAR-GROUP/flecsi2.git ${FLECSI_DIR}
+fi
+if [ ! -d ${SPACK_DIR} ]; then
+  git clone https://github.com/spack/spack.git ${SPACK_DIR}
+  source ${SPACK_DIR}/share/spack/setup-env.sh
+  spack env create -d ${BASE_DIR}
+  spack env activate -p ${BASE_DIR}
+  sed -i 's/unify: false/unify: true/' ${BASE_DIR}/spack.yaml
+  spack repo add ${FLECSI_DIR}/spack-repo/
+  spack external find ninja cmake openmpi python autoconf automake perl m4 hpx ca-certificates-mozilla
+  spack add googletest
+  spack add flecsi%gcc@12.2.0 backend=hpx +flog +unit ^hpx@master ^boost@1.80.0
+  spack concretize -f
+  spack install 
+fi
+
+# build flecsi from source with spack env 
+spack env activate -p ${BASE_DIR}
+
+cmake -S ${FLECSI_DIR} -B ${FLECSI_DIR}/cmake-build_Release -DFLECSI_BACKEND=hpx -DENABLE_FLOG=ON -DENABLE_UNIT_TESTS=ON -DCMAKE_BUILD_TYPE=Release 
+cmake --build ${FLECSI_DIR}/cmake-build_Release/ --parallel
+
+#exit spack
+spack env deactivate #despacktivate
