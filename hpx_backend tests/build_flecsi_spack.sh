94830f84214566cb1cbda9e0d08104fbdf4730cd
Chuanqiu He <49005493+hcq9102@users.noreply.github.com>
2023-05-23 19:25:20 -0500

    Update build_flecsi_spack.sh

--- hpx_backend tests/build_flecsi_spack.sh
+++ hpx_backend tests/build_flecsi_spack.sh
@@ -8,7 +8,8 @@
 
 # set -xe
 #module purge
-module load gcc/12.2.0 cmake/3.23.2 git 
+#module load gcc/12.2.0 cmake/3.23.2 git 
+module load gcc/9.2.1 cmake git
 
 BASE_DIR=$(pwd)
 SPACK_DIR=${BASE_DIR}/spack
@@ -28,16 +29,24 @@ if [ ! -d ${SPACK_DIR} ]; then
   spack repo add ${FLECSI_DIR}/spack-repo/
   spack external find ninja cmake openmpi python autoconf automake perl m4 hpx ca-certificates-mozilla
   spack add googletest
-  spack add flecsi%gcc@12.2.0 backend=hpx +flog +unit ^hpx@master ^boost@1.80.0
+  #spack add flecsi%gcc@12.2.0 backend=hpx +flog +unit ^hpx@master ^boost@1.80.0
+  spack add flecsi%gcc@9.2.1 backend=hpx +flog +unit ^hpx@master ^boost@1.80.0
   spack concretize -f
   spack install 
 fi
 
-# build flecsi from source with spack env 
+#activate spack env
 spack env activate -p ${BASE_DIR}
 
-cmake -S ${FLECSI_DIR} -B ${FLECSI_DIR}/cmake-build_Release -DFLECSI_BACKEND=hpx -DENABLE_FLOG=ON -DENABLE_UNIT_TESTS=ON -DCMAKE_BUILD_TYPE=Release 
-cmake --build ${FLECSI_DIR}/cmake-build_Release/ --parallel
+#build flecsi from source with spack env
+
+#Release type
+cmake -S ${FLECSI_DIR} -B ${FLECSI_DIR}/cmake-build_release -DFLECSI_BACKEND=hpx -DENABLE_FLOG=ON -DENABLE_UNIT_TESTS=ON -DCMAKE_BUILD_TYPE=Release 
+cmake --build ${FLECSI_DIR}/cmake-build_release/ --parallel
+
+#Debug type
+cmake -S ${FLECSI_DIR} -B ${FLECSI_DIR}/cmake-build_debug -DFLECSI_BACKEND=hpx -DENABLE_FLOG=ON -DENABLE_UNIT_TESTS=ON -DCMAKE_BUILD_TYPE=Debug
+cmake --build ${FLECSI_DIR}/cmake-build_debug/ --parallel
 
 #exit spack
 spack env deactivate #despacktivate
